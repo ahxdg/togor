a3fca5345326b6987d74225ccc508078180e2afa
aliyun <1095162930@qq.com>
2016-12-03 17:19:23 +0800

    修改数据库分类字段、添加相应页面

--- Application/Home/Controller/NotesController.class.php
+++ Application/Home/Controller/NotesController.class.php
@@ -45,4 +45,84 @@ class NotesController extends Controller {
       $this->assign('menu', 'notes');
       $this->display();
     }
+	public function web(){
+		 //读取数据库
+       $Articles = M('Article');
+	   $articleList = $Articles->order('id desc')->where('little_class = "web" and big_class=1')->select();
+       $len = count($articleList);
+       for ($i=0; $i < $len; $i++) { 
+         if ($articleList[$i]['time']) {
+           $arr = explode(' ', $articleList[$i]['time'], 2);
+           $articleList[$i]['time'] = $arr[0];
+         }
+       }
+       $this->assign('list',$articleList);
+       $this->assign('nav',$nav);
+      $this->assign('menu', 'notes');
+      $this->display();
+	}
+	public function sql(){
+		 //读取数据库
+       $Articles = M('Article');
+	   $articleList = $Articles->order('id desc')->where('little_class = "sql" and big_class=1')->select();
+       $len = count($articleList);
+       for ($i=0; $i < $len; $i++) { 
+         if ($articleList[$i]['time']) {
+           $arr = explode(' ', $articleList[$i]['time'], 2);
+           $articleList[$i]['time'] = $arr[0];
+         }
+       }
+       $this->assign('list',$articleList);
+       $this->assign('nav',$nav);
+      $this->assign('menu', 'notes');
+      $this->display();
+	}
+	public function other(){
+		 //读取数据库
+       $Articles = M('Article');
+	   $articleList = $Articles->order('id desc')->where('little_class = "other" and big_class=1')->select();
+       $len = count($articleList);
+       for ($i=0; $i < $len; $i++) { 
+         if ($articleList[$i]['time']) {
+           $arr = explode(' ', $articleList[$i]['time'], 2);
+           $articleList[$i]['time'] = $arr[0];
+         }
+       }
+       $this->assign('list',$articleList);
+       $this->assign('nav',$nav);
+      $this->assign('menu', 'notes');
+      $this->display();
+	}
+	public function php(){
+		 //读取数据库
+       $Articles = M('Article');
+	   $articleList = $Articles->order('id desc')->where('little_class = "php" and big_class=1')->select();
+       $len = count($articleList);
+       for ($i=0; $i < $len; $i++) { 
+         if ($articleList[$i]['time']) {
+           $arr = explode(' ', $articleList[$i]['time'], 2);
+           $articleList[$i]['time'] = $arr[0];
+         }
+       }
+       $this->assign('list',$articleList);
+       $this->assign('nav',$nav);
+      $this->assign('menu', 'notes');
+      $this->display();
+	}
+	public function python(){
+		 //读取数据库
+       $Articles = M('Article');
+	   $articleList = $Articles->order('id desc')->where('little_class = "python" and big_class=1')->select();
+       $len = count($articleList);
+       for ($i=0; $i < $len; $i++) { 
+         if ($articleList[$i]['time']) {
+           $arr = explode(' ', $articleList[$i]['time'], 2);
+           $articleList[$i]['time'] = $arr[0];
+         }
+       }
+       $this->assign('list',$articleList);
+       $this->assign('nav',$nav);
+      $this->assign('menu', 'notes');
+      $this->display();
+	}
 }
